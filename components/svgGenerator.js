b3ec481f194553f512a13544fa7e37b93f093d9f
Michal Jadach <michal.jadach@hoprnet.org>
2022-06-15 10:02:14 +0200

    fonts and middleware with geo

--- components/svgGenerator.js
+++ components/svgGenerator.js
@@ -1,15 +1,42 @@
 import { base64 } from '../images_folder/hopr-nft-tokenlon-ama-gold@0.5x-base64'
 
 export default function svgGenerator(title, footer){
-
+                // font-family: "Source Code Pro";
+                // font-style: normal;
     const svg = `<svg width="1601" height="2264" viewBox="0 0 1601 2264" fill="none" xmlns="http://www.w3.org/2000/svg">
         <style>
-            .ip { font: 100px sans-serif; fill: black }
+            .ip { 
+                font-family: "Segoe UI";
+                font-family: "Source Code Pro";
+                font-weight: 400;
+                font-size: 92px; 
+                sans-serif; 
+                fill: black 
+            }
+            .ipv6 { 
+                font-family: "Source Code Pro";
+                font-weight: 400;
+                font-size: 54px; 
+                sans-serif; 
+                fill: black 
+            }
         </style>
         <image href="data:image/jpeg;charset=utf-8;base64,${base64}"  x="0" y="0" width="1601" height="2264" />
         <rect x="350" y="450" width="900" height="1364" fill="rgba(255,255,255,0.8)"></rect>
         <g>
-            <text x="420" y="580" class="ip">255.255.255.254</text>
+            <text x="420" y="580" class="ip">Top IPs:</text>
+            <text x="420" y="690" class="ip">255.255.255.254</text>
+            <text x="420" y="790" class="ip">255.255.255.254</text>
+            <text x="420" y="845" class="ipv6">0000:0000:0000:</text>
+            <text x="420" y="900" class="ipv6">0000:0000:0000:c0a8:64e4</text>
+            <text x="420" y="955" class="ipv6">0000:0000:0000:</text>
+            <text x="420" y="1005" class="ipv6">0000:0000:0000:192.168.100.228</text>
+            <text x="420" y="1090" class="ip">255.255.255.254</text>
+            <text x="420" y="1190" class="ip">255.255.255.254</text>
+            <text x="420" y="1290" class="ip">255.255.255.254</text>
+            <text x="420" y="1390" class="ip">255.255.255.254</text>
+            <text x="420" y="1490" class="ip">255.255.255.254</text>
+            <text x="420" y="1590" class="ip">255.255.255.254</text>
         </g>
         </svg>`;
 
--- pages/api/nft.svg.jpg.js
+++ pages/api/nft.svg.jpg.js
@@ -3,14 +3,19 @@ import path from 'path'
 import { base64 as Gold05base64 }  from '../../images_folder/hopr-nft-tokenlon-ama-gold@0.5x-base64.js'
 import getSvg from "../../components/svgGenerator";
 
+path.resolve(process.cwd(), 'fonts', 'fonts.conf');
+path.resolve(process.cwd(), 'fonts', 'SourceCodePro-Regular.ttf');
+
 import requestIp from 'request-ip'
 const sharp = require('sharp');
 
-const roundedCorners = Buffer.from(getSvg(1,2));
+const buffer = Buffer.from(getSvg(1,2));
 
 export default async function(req, res) {
   const detectedIp = requestIp.getClientIp(req)
 
+  //console.log('req nft', req)
+  console.log('req nft geo', req.geo)
 
   if(detectedIp === "::1"){
     console.log('local')
@@ -24,7 +29,7 @@ export default async function(req, res) {
     mozjpeg: true,
     quality: 100,
   }
-  const output = await sharp(roundedCorners).jpeg(jpeg).toBuffer();
+  const output = await sharp(buffer).jpeg(jpeg).toBuffer();
   
   return res.end(output);
 
